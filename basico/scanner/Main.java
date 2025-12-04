4c9f4f052865be91d62713f27e722a846d8d0e74
matheuscrz238 <matheuscrz348@gmail.com>
2025-12-03 21:01:38 -0300

    Terceiro commit da pasta "basico", exercício "scanner".

---
+++ basico/scanner/Main.java
@@ -0,0 +1,48 @@
+package scanner;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String nome;
+        int idade;
+        String genero;
+        double altura;
+        String profissao;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome: ");
+        nome = scanner.nextLine();
+
+        // Método numérico do Scanner não consome o ENTER, somente "scanner.nextLine();" ou "scanner.next();".
+        System.out.print("Digita a idade: ");
+        idade = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Digite o gênero: ");
+        genero = scanner.nextLine();
+
+        /* O Scanner pode aceitar só vírgula ou ponto. Para isso, recebi a altura como String,
+        substituí a vírgula por ponto e depois converti a String para double, armazenando o resultado
+        na variável altura.
+        */
+        System.out.print("Digite a altura: ");
+        String entrada = scanner.nextLine();
+        entrada = entrada.replace(",", ".");
+        altura = Double.parseDouble(entrada);
+
+        System.out.print("Digite a profissão: ");
+        profissao = scanner.nextLine();
+        System.out.println();
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Gênero: " + genero);
+        System.out.printf("Altura: %.2f\n", altura);
+        System.out.println("Profissão: " + profissao);
+
+        scanner.close();
+    }
+}
