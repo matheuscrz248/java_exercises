5c5df10cd6b02030ae01c43f37abcb977d887282
matheuscrz238 <matheuscrz348@gmail.com>
2025-12-05 20:36:14 -0300

    Sétimo commit da pasta "basico", exercício "random". Imprime números aleatórios de 1 a 9 e mostra "cara" ou "coroa".

---
+++ basico/random/Main.java
@@ -0,0 +1,26 @@
+package random;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int n1;
+        boolean isHead;
+
+        n1 = random.nextInt(1,9);
+        isHead = random.nextBoolean();
+
+        System.out.println("Número aleatório: " + n1);
+
+        if (isHead) {
+            System.out.println("Cara");
+        }
+        else {
+            System.out.println("Coroa");
+        }
+
+    }
+}
