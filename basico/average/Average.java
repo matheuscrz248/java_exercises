ddef95711f4fae174658d5d3a1c6b944aa624550
matheuscrz238 <matheuscrz348@gmail.com>
2026-01-31 22:45:52 -0300

    Add average exercise

---
+++ basico/average/Average.java
@@ -0,0 +1,38 @@
+package average;
+
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double g1, g2, g3, average;
+
+        System.out.println("Enter the first grade:");
+        g1 = scanner.nextDouble();
+
+        System.out.println("Enter the second grade:");
+        g2 = scanner.nextDouble();
+
+        System.out.println("Enter the third grade:");
+        g3 = scanner.nextDouble();
+
+        average = (g1 + g2 + g3) / 3;
+
+        if (average <= 2) {
+            System.out.println("You did not pass");
+        }
+        else if (average < 7) {
+            System.out.println("You are in remedial classes");
+        }
+        else {
+            System.out.println("You passed!");
+        }
+
+        System.out.printf("Your grade average: %.1f%n", average);
+
+        scanner.close();
+    }
+}
+
+
